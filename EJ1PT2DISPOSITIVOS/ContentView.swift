b9d87a84a5423f70363950f47461480ce9be953f
Omar Bermejo Osuna <l21170203@culiacan.tecnm.mx>
2025-09-10 10:05:28 -0700

    Initial Commit

---
+++ EJ1PT2DISPOSITIVOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EJ1PT2DISPOSITIVOS
+//
+//  Created by Omar Bermejo Osuna on 10/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EJ1PT2DISPOSITIVOS/EJ1PT2DISPOSITIVOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EJ1PT2DISPOSITIVOSApp.swift
+//  EJ1PT2DISPOSITIVOS
+//
+//  Created by Omar Bermejo Osuna on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct EJ1PT2DISPOSITIVOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
